bd4d63b832ca8d11fccc68ff047a59ef73691b56
Brian Lemons <brian_lemons@hotmail.com>
2021-06-06 22:52:23 -0700

    adding chal

--- python/tests/test_stacks_and_queues.py
+++ python/tests/test_stacks_and_queues.py
@@ -2,9 +2,9 @@
 
 
 
-def test_pop_single():
-    s = Stack()
-    s.push("apple")
-    actual = s.pop()
-    expectd = "apple"
-    assert actual == expectd
+# def test_pop_single():
+#     s = Stack()
+#     s.push("apple")
+#     actual = s.pop()
+#     expectd = "apple"
+#     assert actual == expectd
