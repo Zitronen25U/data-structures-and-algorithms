ba99f39e635fe7fbde8f4851950e55f96bc79dd6
Brian Lemons <brian_lemons@hotmail.com>
2021-06-30 15:45:24 -0700

    adding

---
+++ python/tests/test_hasmap_left_join.py
@@ -0,0 +1,15 @@
+from python.hasmap_left_join.hashmap_left_join import left_join, Hashtable
+
+ht1 = Hashtable()
+    ht2 = Hashtable()
+    ht1.add('fond', 'enamored')
+    ht1.add('wrath', 'anger')
+    ht1.add('diligent', 'employed')
+    ht1.add('outfit', 'garb')
+    ht1.add('guide', 'usher')
+    ht2.add('fond', 'averse')
+    ht2.add('wrath', 'delight')
+    ht2.add('diligent', 'idle')
+    ht2.add('guide', 'follow')
+    ht2.add('flow', 'jam')
+
